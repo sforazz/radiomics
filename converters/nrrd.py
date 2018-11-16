aaa04caefae44d274c08673d07d9696dad454c50
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2018-11-16 17:45:33 +0100

    added basic function to convert images from nrrd to nifti. It works ok
    but it does not preserve the original image orientation so be carefull.

---
+++ converters/nrrd.py
@@ -0,0 +1,23 @@
+import nrrd
+import nibabel as nib
+import numpy as np
+from utils.filemanip import split_filename
+import os
+
+
+class NrrdConverters():
+    
+    def __init__(self, nrrd_file, ext='.nii.gz', outname=None):
+        
+        self.nrrd_file = nrrd_file
+        path, filename, _ = split_filename(nrrd_file)
+        if outname is None:
+            self.outname = os.path.join(path, filename)+ext
+        else:
+            self.outname = outname
+    
+    def nrrd2nifti(self):
+        
+        data, _ = nrrd.read(self.nrrd_file)
+        tosave = nib.Nifti1Image(data, np.eye(4))
+        nib.save(tosave, self.outname)
