bde65cf3f38ecbfce72a15a95c9c3205ab30ed34
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2020-03-17 15:39:57 +0100

    adding new features to modelling

--- scripts/modelling_r2py.py
+++ scripts/modelling_r2py.py
@@ -45,7 +45,7 @@ string_univariate="""
       wald.test <- paste0(signif(x$wald["test"], digits = 2), 
                           " (", 
                           HR.confint.lower, 
-                          "-", 
+                          "_", 
                           HR.confint.upper, 
                           ")") 
       res <- c(beta, HR, wald.test, p.value) #save results 
@@ -72,10 +72,11 @@ Data = Data[[c for c in Data if c not in ['PID', 'TIME', 'STATUS']]]
 corr_matrix = Data.corr().abs()
 upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
 to_drop = [column for column in upper.columns if any(upper[column] > 0.9)]
+z = 0
 while to_drop:
     to_remove = []
     pval_check = []
-    col_index = to_drop[0]
+    col_index = to_drop[z]
     corr_index = [upper.columns[x[0]] for x in np.where(upper[col_index] > 0.9)]
     sub_data = Data[[c for c in Data if c in corr_index+[col_index]]]
     r_sub_data = converter(sub_data, r2pandas=False)
@@ -86,7 +87,7 @@ while to_drop:
     if hr:
         if len(hr) < len(corr_index+[col_index]):
             to_remove = to_remove + [x for x in corr_index+[col_index] if x not in hr]
-        wald = [[key]+re.sub('[()]', '', value.split()[-1]).split('-')
+        wald = [[key]+re.sub('[()]', '', value.split()[-1]).split('_')
                 for key, value in res['wald.test'].items() if key in hr]
         for wald_range in wald:
             key, low, high = wald_range
@@ -98,17 +99,19 @@ while to_drop:
             p_values = [[key, value] for key, value in res['p.value'].items()
                         if key in pval_check and float(value) < 0.1]
             if p_values and len(p_values) > 1:
-                values = [x[1] for x in p_values]
+                values = [float(x[1]) for x in p_values]
                 min_pval = np.min(values)
                 temp_to_remove = [p_values[i][0] for i, val in enumerate(values)
-                                  if val != min_pval][0]
+                                  if val != min_pval]
                 to_remove = to_remove + temp_to_remove
             elif not p_values:
                 to_remove = to_remove + pval_check
-        if to_remove:
-            Data = Data[[c for c in Data if c not in to_remove]]
     else:
-        Data = Data[[c for c in Data if c not in corr_index+[col_index]]]
+        to_remove = corr_index+[col_index]
+    if to_remove:
+        Data = Data[[c for c in Data if c not in to_remove]]
+    else:
+        z = z+1
     corr_matrix = Data.corr().abs()
     upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
     to_drop = [column for column in upper.columns if any(upper[column] > 0.9)]
